6b6c1655493f0cf5076d55390c23ba70d22cf5f7
yzlhdy <970716974@qq.com>
2020-07-21 23:34:33 +0800

    提交

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { Container } from './styles';
 
 const ProfilePage: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Banner></Banner>
+    </Container>
+  );
 };
 
 export default ProfilePage;
