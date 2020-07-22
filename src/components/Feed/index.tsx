7cd05a495b5c6b4af7de416aacab82794a079305
yzlhdy <970716974@qq.com>
2020-07-22 14:06:30 +0800

    头部个人信息西展示完成

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container, Tab } from './styles';
+
+const Feed: React.FC = () => {
+    return (
+        <Container>
+            <Tab>Tweets</Tab>
+            {/* <Tweets>
+                <Tweet />
+                <Tweet />
+                <Tweet />
+            </Tweets> */}
+        </Container>
+    );
+}
+
+export default Feed;
--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
-
-import { Container } from './styles';
+import Feed from '../Feed'
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton
+} from './styles';
 
 const ProfilePage: React.FC = () => {
   return (
     <Container>
-      <Banner></Banner>
+      {/* 头部 */}
+      <Banner>
+        <Avatar />
+      </Banner>
+      <ProfileData>
+        <EditButton outlined> Edit fill12313</EditButton>
+        <h1>Guilherme Rodz</h1>
+        <h2>@guilherme_rodz</h2>
+        <p>
+          Developer at <a href="https://www.baidu.com">@yizhiyang</a>
+        </p>
+        <ul>
+
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            São Paulo, Brasil
+          </li>
+        </ul>
+        <Followage>
+          <span>
+            São Paulo<strong>90</strong>
+          </span>
+          <span>
+            <strong>90</strong> Brasil
+          </span>
+        </Followage>
+      </ProfileData>
+      <Feed></Feed>
+
     </Container>
   );
 };
